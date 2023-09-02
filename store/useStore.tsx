5a2888bb21dd8e4372f4066266c8e134f1b9454e
ultraviolet10 <aritrac1998@hotmail.com>
2023-09-02 22:04:49 +0530

    update: use non deprecated import

--- store/useStore.tsx
+++ store/useStore.tsx
@@ -1,12 +1,17 @@
-import create from "zustand";
+import { create } from "zustand";
 
 type JsonStoreState = {
+  input: Record<string, any>;
+  setInput: (newSchema: Record<string, any>) => void;
   schema: Record<string, any>;
   setSchema: (newSchema: Record<string, any>) => void;
 };
 
 const useJsonStore = create<JsonStoreState>((set) => ({
+  input: {},
   schema: {},
+  // setters
+  setInput: (newInput) => set({ input: newInput }),
   setSchema: (newSchema) => set({ schema: newSchema }),
 }));
 
